20e15cc96b0483c51941a252f7b7e5ec64633063
Mohamed-Zakaria SAIDANE <mohamedzakariasaidane@gmail.com>
2021-02-08 04:08:11 +0100

    major modification of the estimate service

--- src/main/java/com/medzaksdn/ridepriceestimator/service/impl/EstimateServiceImpl.java
+++ src/main/java/com/medzaksdn/ridepriceestimator/service/impl/EstimateServiceImpl.java
@@ -1,19 +1,21 @@
 package com.medzaksdn.ridepriceestimator.service.impl;
 
-import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import com.google.maps.GaeRequestHandler;
 import com.google.maps.DistanceMatrixApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.errors.ApiException;
+import com.google.maps.model.DistanceMatrix;
 import com.google.maps.model.DistanceMatrixRow;
+import com.google.maps.model.LatLng;
 import com.google.maps.model.TravelMode;
 import com.google.maps.model.Unit;
 import com.medzaksdn.ridepriceestimator.dto.EstimateDTO;
@@ -45,56 +47,62 @@ public class EstimateServiceImpl implements EstimateService {
         List<PriceDTO> priceDtos = new ArrayList<>();
         List<RideOption> rideOptions = rideOptionJpaRepository.findAll();
 
-        for (RideOption rideOption : rideOptions) {
-            priceDtos.add(priceCalculate(getDistance(estimateDTO), rideOption));
+        Long distance = getDistance(estimateDTO);
+        if (distance != null) {
+            for (RideOption rideOption : rideOptions) {
+                priceDtos.add(priceCalculate(distance, rideOption));
+            }
         }
 
         return priceDtos;
     }
 
     private Long getDistance(EstimateDTO estimateDTO) {
-        GeoApiContext geoApiContext = new GeoApiContext.Builder(new GaeRequestHandler.Builder())
+        GeoApiContext geoApiContext = new GeoApiContext.Builder()
                 .apiKey(this.googleAPIKey)
                 .build();
 
         try {
-            DistanceMatrixRow rows[] = DistanceMatrixApi
-                    .getDistanceMatrix(geoApiContext, new String[]{estimateDTO.latitude1, estimateDTO.longitude1}, new String[]{estimateDTO.latitude2, estimateDTO.longitude2})
+            DistanceMatrix matrix = DistanceMatrixApi.newRequest(geoApiContext)
+                    .origins(new LatLng(Double.parseDouble(estimateDTO.latitude1), Double.parseDouble(estimateDTO.longitude1)))
+                    .destinations(new LatLng(Double.parseDouble(estimateDTO.latitude2), Double.parseDouble(estimateDTO.longitude2)))
                     .units(Unit.METRIC)
-                    .mode(TravelMode.DRIVING).await().rows;
+                    .mode(TravelMode.DRIVING).await();
+
+            DistanceMatrixRow rows[] = matrix.rows;
 
             // We have only one element
             return rows[0].elements[0].distance.inMeters;
 
-            // For more take a look at
-            // the Google Maps Distance Matrix API documentation :
+            // For more take a look at the Google Maps Distance Matrix API documentation :
             // https://developers.google.com/maps/documentation/distance-matrix/overview
         } catch (ApiException e) {
-            System.err.println(e.getMessage());
+            e.printStackTrace();
         } catch (InterruptedException e) {
-            System.err.println(e.getMessage());
+            e.printStackTrace();
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            e.printStackTrace();
         }
 
         return null;
     }
 
     private PriceDTO priceCalculate(Long distance, RideOption rideOption) {
-        List<Price> prices = priceJpaRepository.findByRideOption(rideOption, Sort.by(Sort.Direction.ASC, "distance"));
+        List<Price> prices = priceJpaRepository.findByRideOption_Id(rideOption.getId(), Sort.by(Sort.Direction.ASC, "distance"));
 
         Double totalPrice = new Double(0);
         Price before = prices.get(0);
         for (Price price : prices) {
-            if (distance - price.getDistance() >= 0) {
-                totalPrice = totalPrice + (price.getDistance() - before.getDistance()) * price.getPrice();
+            if (distance >= price.getDistance()) {
+                totalPrice = totalPrice + (price.getDistance() - before.getDistance()) * price.getPrice() / 1000;
             } else {
-                totalPrice = totalPrice + (distance - before.getDistance()) * price.getPrice();
+                totalPrice = totalPrice + (distance - before.getDistance()) * price.getPrice() / 1000;
+                break;
             }
         }
 
-        // minimum price
-        Price minimumPrice = priceJpaRepository.findOneByRideOptionAndDistance(rideOption, new Double(0));
+        // get minimum price
+        Price minimumPrice = priceJpaRepository.findByRideOption_IdAndDistance(rideOption.getId(), 0);
 
         totalPrice = totalPrice > minimumPrice.getPrice() ? totalPrice : minimumPrice.getPrice();
 
